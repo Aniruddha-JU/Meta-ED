01189a140fff09c98badbb23cf21dd0e7369271a
Aniruddha Roy <aniruddharoy.ju@gmail.com>
2024-06-19 17:35:24 +0530

    changes

---
+++ sed.sh
@@ -0,0 +1,22 @@
+sed -i 's/B-SEISMIC_RISK/O/g' data/mar/95shot.txt
+sed -i 's/B-SEISMIC_RISK/O/g' data/hi/95shot.txt
+sed -i 's/B-SEISMIC_RISK/O/g' data/ben/95shot.txt
+sed -i 's/B-PANDEMIC/O/g' data/te/95shot.txt
+sed -i 's/B-PANDEMIC/O/g' data/mar/95shot.txt
+sed -i 's/B-PANDEMIC/O/g' data/ben/95shot.txt
+sed -i 's/B-PANDEMIC/O/g' data/hi/95shot.txt
+sed -i 's/I-PANDEMIC/O/g' data/hi/95shot.txt
+sed -i 's/I-PANDEMIC/O/g' data/ben/95shot.txt
+sed -i 's/I-PANDEMIC/O/g' data/mar/95shot.txt
+sed -i 's/I-PANDEMIC/O/g' data/te/95shot.txt
+sed -i 's/B-SEISMIC_RISK/O/g' data/te/95shot.txt
+
+
+
+
+
+
+
+
+
+
---
+++ sed1.sh
@@ -0,0 +1,23 @@
+sed -i 's/B-SEISMIC_RISK/O/g' data/te/4k.txt
+sed -i 's/B-SEISMIC_RISK/O/g' data/mar/4k.txt
+sed -i 's/B-SEISMIC_RISK/O/g' data/hi/4k.txt
+sed -i 's/B-SEISMIC_RISK/O/g' data/ben/4k.txt
+sed -i 's/B-PANDEMIC/O/g' data/te/4k.txt
+sed -i 's/B-PANDEMIC/O/g' data/mar/4k.txt
+sed -i 's/B-PANDEMIC/O/g' data/ben/4k.txt
+sed -i 's/B-PANDEMIC/O/g' data/hi/4k.txt
+sed -i 's/I-PANDEMIC/O/g' data/hi/4k.txt
+sed -i 's/I-PANDEMIC/O/g' data/ben/4k.txt
+sed -i 's/I-PANDEMIC/O/g' data/mar/4k.txt
+sed -i 's/I-PANDEMIC/O/g' data/te/4k.txt
+
+
+
+
+
+
+
+
+
+
+
